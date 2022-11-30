16bfdc8c237c5c7b765f75a2a9c1a2489c8cfed8
fertilian <fertiliast2002@gmail.com>
2022-11-30 14:25:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -163,7 +163,7 @@
                             </div>
                         </div>
                         <div class="carousel-item" style="height: 410px;">
-                            <img class="img-fluid" src="img/carousel-2.jpg" alt="Image">
+                            <img class="img-fluid" src="assets/img/toko-komputer.jpg" alt="Image">
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
                                     <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
