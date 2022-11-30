b88578d7891634a3c257c9fc3d1e044d788cdd25
fertilian <fertiliast2002@gmail.com>
2022-11-30 14:15:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -156,8 +156,8 @@
                             <img class="img-fluid" src="assets/img/carousel-background.jpg" alt="Image">
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
-                                    <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
-                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Fashionable Dress</h3>
+                                    <h4 class="text-light text-uppercase font-weight-medium mb-3">Diskon 10% bagi pelanggan pertama</h4>
+                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">SITTOK</h3>
                                     <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
                                 </div>
                             </div>
