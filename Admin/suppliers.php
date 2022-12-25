f978a66fd54274c23b91654505cc937ccb2cafa4
Kresna1609 <kresna.rizky113@gmail.com>
2022-12-25 13:54:33 +0700

    Update suppliers.php

--- Admin/suppliers.php
+++ Admin/suppliers.php
@@ -132,25 +132,25 @@
             <div class="col-lg-12">
               <div class="card mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Form Basic</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Data Master Suppliers</h6>
                 </div>
                 <div class="card-body">
                   <form>
                     <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
+                      <label for="exampleInputEmail1">User Email</label>
                       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     </div>
 
                     <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
+                      <label for="exampleInputEmail1">User Email</label>
                       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     </div>
 
                     <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
+                      <label for="exampleInputEmail1">User Email</label>
                       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     </div>
-                    
+
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </form>
                 </div>
