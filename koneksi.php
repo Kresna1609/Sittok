829285cb7e76e76db81d87a31a820bafd2abf48c
Kresna1609 <kresna.rizky113@gmail.com>
2022-12-25 12:32:12 +0700

    p

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$db = "tkkomputer";
+$db = "sittok";
 $koneksi = mysqli_connect($server, $username, $password, $db);
 
 if(mysqli_connect_errno()) {
