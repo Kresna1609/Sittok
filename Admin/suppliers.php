2e38ece1b477b975a5bf2acb26be4c6c561aa6d4
Kresna1609 <kresna.rizky113@gmail.com>
2022-12-25 13:38:53 +0700

    Update suppliers.php

--- Admin/suppliers.php
+++ Admin/suppliers.php
@@ -140,10 +140,24 @@
                       <label for="exampleInputEmail1">Email address</label>
                       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     </div>
+
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                    
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </form>
                 </div>
               </div>
+              
+
+            
         <!-- <Form Basic> -->
       </div>
     </div>
