eb8ea7ca3c890c17bf774a96c87feac148d6ec65
Kresna1609 <kresna.rizky113@gmail.com>
2022-12-26 14:51:05 +0700

    p

--- Admin/barang/list.php
+++ Admin/barang/list.php
@@ -9,9 +9,9 @@
   <meta name="author" content="">
   <link href="img/logo/logo.png" rel="icon">
   <title>SITTOK</title>
-  <link href="Admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="Admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="Admin/css/ruang-admin.min.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/ruang-admin.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -154,12 +154,12 @@
     <i class="fas fa-angle-up"></i>
   </a>
 
-  <script src="Admin/vendor/jquery/jquery.min.js"></script>
-  <script src="Admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="Admin/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="Admin/js/ruang-admin.min.js"></script>
-  <script src="Admin/vendor/chart.js/Chart.min.js"></script>
-  <script src="Admin/js/demo/chart-area-demo.js"></script>  
+  <script src="Admin/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="Admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="Admin/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="Admin/assets/js/ruang-admin.min.js"></script>
+  <script src="Admin/assets/vendor/chart.js/Chart.min.js"></script>
+  <script src="Admin/assets/js/demo/chart-area-demo.js"></script>  
 </body>
 
 </html>
