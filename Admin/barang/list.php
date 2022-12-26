436be74d152f8f2a1ca2f798cfec2340198b9ff7
fertilian <fertiliast2002@gmail.com>
2022-12-26 17:21:01 +0700

    Update list.php

--- Admin/barang/list.php
+++ Admin/barang/list.php
@@ -87,7 +87,7 @@ require('koneksi.php');
         <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Data Master Customers</h1>
+            <h1 class="h3 mb-0 text-gray-800">Data Master Barang</h1>
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="./">Home</a></li>
               <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
@@ -100,7 +100,7 @@ require('koneksi.php');
             <div class="col-lg-12">
               <div class="card mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Data Master Customers</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Data Master Barang</h6>
                 </div>
                 <div class="card-body">
                             <div class="table-responsive">
