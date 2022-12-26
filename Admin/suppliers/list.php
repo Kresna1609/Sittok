fe29de3c00e1a71dbe20bcab1fb029add05fd661
karinadewii <afkarinadewikha@gmail.com>
2022-12-26 16:39:23 +0700

    pusing

--- Admin/suppliers/list.php
+++ Admin/suppliers/list.php
@@ -83,7 +83,7 @@
         <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Data Master Customers</h1>
+            <h1 class="h3 mb-0 text-gray-800">Data Master Suppliers</h1>
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="./">Home</a></li>
               <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
@@ -96,7 +96,7 @@
             <div class="col-lg-12">
               <div class="card mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Data Master Customers</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Data Master Suppliers</h6>
                 </div>
                 <div class="card-body">
                             <div class="table-responsive">
