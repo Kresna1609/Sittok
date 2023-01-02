d27630a7a776308e5553dcba9609d61e3d535b38
Kresna1609 <kresna.rizky113@gmail.com>
2023-01-02 23:25:14 +0700

    Update shop.php

--- shop.php
+++ shop.php
@@ -57,10 +57,10 @@ $id = $_GET['id_kategori'];
                        
                             while($shop = $sql->fetch_array()){
                                 ?>
-                                <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                                <div class="col-lg-2 col-md-6 col-sm-6 pb-1">
                                 <div class="card product-item border-0 mb-4">
                                     <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                        <img class="img-fluid w-100" src="admin/assets/img/barang/<?php echo $shop['gambar']; ?>" alt="">
+                                        <img class="img-fluid w-65" src="admin/assets/img/barang/<?php echo $shop['gambar']; ?>" width="300px" height="300px">
                                     </div>
                                     <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
                                         <h6 class="text-truncate mb-3"><?=$shop['merk_barang']?></h6>
