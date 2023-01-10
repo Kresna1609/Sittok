5dadfffa00a72e7601b59d4334da50302687eb98
fertilian <fertiliast2002@gmail.com>
2023-01-10 14:19:25 +0700

    dikit

--- cart.php
+++ cart.php
@@ -61,7 +61,7 @@ $grand_total = 0;
         <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
             <h1 class="font-weight-semi-bold text-uppercase mb-3">Keranjang</h1>
             <div class="d-inline-flex">
-                <p class="m-0"><a href="">Home</a></p>
+                <p class="m-0"><a href="index.php">Home</a></p>
                 <p class="m-0 px-2">-</p>
                 <p class="m-0">Shopping Cart</p>
             </div>
